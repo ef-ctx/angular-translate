64c49127595e38dba6637be8187581b7d81a7b90
Jaime Beneytez <jaimebeneytezduran@gmail.com>
2014-08-01 18:13:17 +0100

    chore: cleaning features we don't need

--- src/service/storage-key.js
+++
@@ -1,2 +0,0 @@
-ngTranslate
-.constant('$STORAGE_KEY', 'NG_TRANSLATE_LANG_KEY');
--- templates/tpl.js
+++
@@ -1,8 +0,0 @@
-/* HTML templates */
-tagsInput.run(function($templateCache) {
-  'use strict';
-
-  $templateCache.put('templates/localeSelector.tpl.html',
-    "<div class=\"btn-group\"><button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">Language <span class=\"caret\"></span></button><ul class=\"dropdown-menu\" role=\"menu\"><li data-ng-repeat=\"locale in localeCollection\"><span class=\"locale-{{ locale.key }}\" data-translate=\"common.localeSelector\" ng-click=\"changeLocale({{locale.key}})\"></span></li></ul></div>"
-  );
-});
